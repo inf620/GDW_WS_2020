39e5aaa50b9e1f5fe11b8c480073776f2938b61f
bamalamusic <Emre@users.noreply.github.com>
2020-12-10 03:13:32 +0100

    poc und modules

---
+++ Projekt/index.js
@@ -0,0 +1,37 @@
+const exampleTrackID ='xxx';
+
+
+const SpotifyWebApi = require('./node_modules/spotify-web-api');
+
+const   spotifyApi = new SpotifyWebApi({
+        clientId: '<insert client id>',
+        clientSecret: '<insert client secret>',
+});
+
+
+
+spotifyApi.getAccessToken().then( //token wird geholt
+    function(data) {
+      console.log('The access token expires in ' + data.body['expires_in']);
+      console.log('The access token is ' + data.body['access_token']);
+  
+    
+      spotifyApi.setAccessToken(data.body['access_token']); //token wird gespeichert
+    },
+    function(err) {
+      console.log(err);
+    }
+  );
+  
+  spotifyApi.getTrack('exampleTrackID').then((function(data){
+    console.log('[Track Info] Name', data.body ,'Artist', data.artistname );
+    }, function(err){
+
+        console.log(err)
+
+    }))
+
+
+
+
+
